e6de66a4ecfd610231af9e432f925b3ebd3b19b0
BeatrizDile <beatriz.dile@gmail.com>
2022-11-03 16:38:44 -0300

    Other website

--- league-tier-list/main.py
+++ league-tier-list/main.py
@@ -6,15 +6,12 @@ chrome_driver_path = MY_CHROME_DRIVER_PATH
 
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
-driver.get("https://u.gg/lol/jungle-tier-list")
+driver.get("https://www.leagueofgraphs.com/pt/champions/builds/jungle/by-winrate")
 
-champs_names = driver.find_elements(By.CLASS_NAME, 'champion-name')
+champs_names = driver.find_elements(By.CLASS_NAME, 'name')
 num_of_champs = [i for i, n in enumerate(champs_names)]
-tier_champs = driver.find_elements(By.CSS_SELECTOR, "b")
 
 
-for rank in num_of_champs:
-    print(rank+1)
-    print(champs_names[rank].get_attribute("textContent"))
-    print(tier_champs[rank].get_attribute("textContent"))
+for i in num_of_champs:
+    print(i, champs_names[i].text)
 
