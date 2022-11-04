916a20040e92669df68871fca465f9a7283d019a
BeatrizDile <beatriz.dile@gmail.com>
2022-11-04 12:28:19 -0300

    Adding data

--- league-tier-list/main.py
+++ league-tier-list/main.py
@@ -6,12 +6,15 @@ chrome_driver_path = MY_CHROME_DRIVER_PATH
 
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
-driver.get("https://www.leagueofgraphs.com/pt/champions/builds/jungle/by-winrate")
+driver.get("https://www.leagueofgraphs.com/champions/builds/jungle/by-winrate")
 
 champs_names = driver.find_elements(By.CLASS_NAME, 'name')
 num_of_champs = [i for i, n in enumerate(champs_names)]
-
+champs_data = driver.find_elements(By.CLASS_NAME, 'progressBarTxt')
 
 for i in num_of_champs:
     print(i, champs_names[i].text)
+    print(f"Popularity: {champs_data[i*3].text} | Winrate: {champs_data[(i*3)+1].text} | Banrate: {champs_data[(i*3)+2].text}")
+
+
 
