bb8c107d7f245ecb79859fe33cffbff274c5fda3
BeatrizDile <beatriz.dile@gmail.com>
2022-11-05 16:32:24 -0300

    Better lists

--- league-tier-list/main.py
+++ league-tier-list/main.py
@@ -9,11 +9,13 @@ driver = webdriver.Chrome(executable_path=chrome_driver_path)
 driver.get("https://www.leagueofgraphs.com/champions/builds/jungle/by-winrate")
 
 champs_names = driver.find_elements(By.CLASS_NAME, 'name')
+champs_names = [name for i, name in enumerate(champs_names) if i != 0]
 num_of_champs = [i for i, n in enumerate(champs_names)]
 champs_data = driver.find_elements(By.CLASS_NAME, 'progressBarTxt')
 
+
 for i in num_of_champs:
-    print(i, champs_names[i].text)
+    print(i+1, champs_names[i].text)
     print(f"Popularity: {champs_data[i*3].text} | Winrate: {champs_data[(i*3)+1].text} | Banrate: {champs_data[(i*3)+2].text}")
 
 
