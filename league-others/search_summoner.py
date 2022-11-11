cc04ce1828740efff2ab23e87f5a1e7feb2bd443
BeatrizDile <beatriz.dile@gmail.com>
2022-11-11 15:04:08 -0300

    Reject button

--- league-others/search_summoner.py
+++ league-others/search_summoner.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-chrome_driver_path = "C:/Users/beatr/Development/chromedriver_win32/chromedriver.exe"
+chrome_driver_path = MY_CHROME_DRIVER_PATH
 
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
@@ -21,6 +21,8 @@ def search_summoner(nick):
         summoner = driver.find_element(By.XPATH, '//*[@id="mainContent"]/div/div[2]/div/table/tbody/tr[2]/td[2]/div/div[2]/a')
         summoner.click()
 
+    reject_all = driver.find_element(By.XPATH, '//*[@id="ncmp__tool"]/div/div/div[3]/div[1]/a')
+    reject_all.click()
     current_url = driver.current_url
     print(current_url)
     driver.save_screenshot(f"images/summoner_print.png")
