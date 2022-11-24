158f216c034eae539ae7b583b73d2e84eabf81cc
BeatrizDile <beatriz.dile@gmail.com>
2022-11-24 16:21:39 -0300

    Data & Interface

--- league-infos/interface.py
+++ league-infos/interface.py
@@ -1,8 +1,7 @@
-import tkinter as tk
+import json
 from tkinter import *
 
-
-root = tk.Tk()
+root = Tk()
 root.title("League Tier List")
 root.geometry("600x600")
 champs = []
@@ -13,7 +12,7 @@ main_frame.pack(fill=BOTH, expand=1)
 canvas = Canvas(main_frame)
 canvas.pack(side=LEFT, fill=BOTH, expand=1)
 
-scrollbar = tk.Scrollbar(main_frame, orient=VERTICAL, command=canvas.yview)
+scrollbar = Scrollbar(main_frame, orient=VERTICAL, command=canvas.yview)
 scrollbar.pack(side=RIGHT, fill=Y)
 
 canvas.configure(yscrollcommand=scrollbar.set)
@@ -23,16 +22,22 @@ second_frame = Frame(canvas)
 canvas.create_window((0, 0), window=second_frame, anchor="nw")
 images = []
 
-
-tk.Label(second_frame,
-          text="League of Legends Tier List",
-          font=("Times New Roman", 15)).grid(column=1, row=0, pady=20, padx=100)
-
+Label(second_frame,
+      text="League of Legends Jungle Tier List",
+      font=("Times New Roman", 15)).grid(column=1, row=0, pady=20, padx=100)
 
 for i in range(45):
     images.append(PhotoImage(file=f"images/champ{i}.png"))
     Label(second_frame, image=images[i], width=60, height=60).grid(pady=20, padx=10)
 
+    with open("data/champs_jungle.json", "r") as file:
+        champs_data = json.load(file)
+
+    champs_data = champs_data["champs"]
+
+    Label(second_frame,
+          text=f"{i + 1}. {champs_data[i]['name']} \nPopularity: {champs_data[i]['popularity']} | Winrate: {champs_data[i]['winrate']} | Banrate: {champs_data[i]['banrate']}",
+          font=("Ariel", 10), anchor="w").grid(column=1, row=i + 1)
 
 root.mainloop()
 
--- league-infos/tier_list.py
+++ league-infos/tier_list.py
@@ -1,9 +1,9 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+import json
 
 chrome_driver_path = MY_CHROME_DRIVER_PATH
-
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
 driver.get("https://www.leagueofgraphs.com/champions/builds/jungle/by-winrate")
@@ -16,8 +16,49 @@ champs_names = [name for i, name in enumerate(champs_names) if i != 0]
 num_of_champs = [i for i, n in enumerate(champs_names)]
 champs_data = driver.find_elements(By.CLASS_NAME, 'progressBarTxt')
 
+
+def multiple_tabs():
+    champ_in_url = [champs.text.lower().replace("&", "").replace("'", "").replace(".", "").replace(" ", "") for champs in champs_names]
+
+    for i, champ in enumerate(champ_in_url):
+        print(champ)
+        if champ == "wukong":
+            champ = "monkeyking"
+        if champ == "nunuwillump":
+            champ = "nunu"
+        driver.execute_script("window.open('');")
+        driver.switch_to.window(driver.window_handles[i+1])
+        driver.get(f'https://www.leagueofgraphs.com/champions/builds/{champ}/jungle')
+
+        with open(f'images/champ{i}.png', 'wb') as file:
+            file.write(driver.find_element(By.XPATH,
+                '//*[@id="pageContent"]/div[1]/div/div[1]/img').screenshot_as_png)
+
+
+with open('data/champs_jungle.json', "w") as empty_file:
+    empty = {"champs": []}
+    json.dump(empty, empty_file)
+
+
 for i in num_of_champs:
     print(i+1, champs_names[i].text)
     print(f"Popularity: {champs_data[i*3].text} | Winrate: {champs_data[(i*3)+1].text} | Banrate: {champs_data[(i*3)+2].text}")
 
+    champ_info = {
+                "name": champs_names[i].text,
+                "popularity": str(champs_data[i*3].text),
+                "winrate": str(champs_data[(i*3)+1].text),
+                "banrate": str(champs_data[(i*3)+2].text)
+                }
+
+    with open('data/champs_jungle.json', "r") as old_file:
+        file_champ_info = json.load(old_file)
+
+    file_champ_info["champs"].append(champ_info)
+
+    with open('data/champs_jungle.json', 'w') as file:
+        json.dump(file_champ_info, file, indent=4)
+
+
+driver.quit()
 
